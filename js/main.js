a53727ec6039f83bb1128b2afdf5c8f84667a54d
DiabloDraco <mirmuhsinmirahmatov@gmail.com>
2022-08-27 23:18:00 +0500

    add

--- js/main.js
+++ js/main.js
@@ -1,21 +1,36 @@
 let elInput = document.querySelector(".header__input")
 let elTemplate = document.querySelector("#bookTem").content
 let elWrapper = document.querySelector(".books__list")
+let elResult = document.querySelector(".result__num")
+let elForm = document.querySelector(".header__input-wrapper")
 
+elForm.addEventListener("submit" , function (evt) {
+    evt.preventDefault()
 
-elInput.addEventListener("input" , function (evt) {
     fetch(`https://www.googleapis.com/books/v1/volumes?q=${elInput.value}`)
     .then(req => req.json())
-    .then(data => console.log(data))
+    .then(data => {
+        render(data.items)
+        elResult.textContent = data.totalItems
+    })
 })
 
 function render(array) {
     elWrapper.innerHTML = null
     let fragment = document.createDocumentFragment()
-
     for (let i = 0; i < array.length; i++) {
         let template = elTemplate.cloneNode(true)
 
+        template.querySelector(".books__top-img").src = array[i].volumeInfo.imageLinks.thumbnail
+        template.querySelector(".books__item-heading").textContent = array[i].volumeInfo.title
+        template.querySelector(".books__item-auth").textContent = array[i].volumeInfo.authors
+        template.querySelector(".books__item-year").textContent = array[i].volumeInfo.publishedDate
+        template.querySelector(".books__bookmark-btn").dataset.bookmarkId = array[i].id
+        template.querySelector(".books__read-btn").href = array[i].accessInfo.webReaderLink
+        template.querySelector(".books__info-btn").dataset.infoId = array[i].id
+        template.querySelector(".books__read-btn").dataset.readId = array[i].id
         
+        fragment.appendChild(template)
     }
+    elWrapper.appendChild(fragment)
 }
