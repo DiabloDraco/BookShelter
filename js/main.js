8509c33cc1adef0ad5e1b042e6aa1e23f0463139
DiabloDraco <mirmuhsinmirahmatov@gmail.com>
2022-08-28 15:50:49 +0500

    da

--- js/main.js
+++ js/main.js
@@ -19,36 +19,40 @@ if (localSaved) {
 elForm.addEventListener("submit" , function (evt) {
     evt.preventDefault()
     
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=${elInput.value}`)
-    .then(req => req.json())
-    .then(data => {
-        render(data.items)
-        elResult.textContent = data.totalItems
-    })
+    if (elInput.value.length > 1) {
+        fetch(`https://www.googleapis.com/books/v1/volumes?q=${elInput.value}`)
+        .then(req => req.json())
+        .then(data => {
+            render(data.items)
+            elResult.textContent = data.totalItems
+        })
+    }
 })
 
 function render(array) {
-    elWrapper.innerHTML = null
-    let fragment = document.createDocumentFragment()
-    for (let i = 0; i < array.length; i++) {
-        let template = elTemplate.cloneNode(true)
-        
-        if (array[i].volumeInfo.imageLinks) {
-            template.querySelector(".books__top-img").src = array[i].volumeInfo.imageLinks.thumbnail
-        }else{
-            template.querySelector(".books__top-img").src = "https://picsum.photos/453/300"
+    if (array) {
+        elWrapper.innerHTML = null
+        let fragment = document.createDocumentFragment()
+        for (let i = 0; i < array.length; i++) {
+            let template = elTemplate.cloneNode(true)
+            
+            if (array[i].volumeInfo.imageLinks) {
+                template.querySelector(".books__top-img").src = array[i].volumeInfo.imageLinks.thumbnail
+            }else{
+                template.querySelector(".books__top-img").src = "https://picsum.photos/453/300"
+            }
+            template.querySelector(".books__item-heading").textContent = array[i].volumeInfo.title
+            template.querySelector(".books__item-auth").textContent = array[i].volumeInfo.authors
+            template.querySelector(".books__item-year").textContent = array[i].volumeInfo.publishedDate
+            template.querySelector(".books__bookmark-btn").dataset.bookmarkId = array[i].id
+            template.querySelector(".books__read-btn").href = array[i].accessInfo.webReaderLink
+            template.querySelector(".books__info-btn").dataset.infoId = array[i].id
+            template.querySelector(".books__read-btn").dataset.readId = array[i].id
+            
+            fragment.appendChild(template)
         }
-        template.querySelector(".books__item-heading").textContent = array[i].volumeInfo.title
-        template.querySelector(".books__item-auth").textContent = array[i].volumeInfo.authors
-        template.querySelector(".books__item-year").textContent = array[i].volumeInfo.publishedDate
-        template.querySelector(".books__bookmark-btn").dataset.bookmarkId = array[i].id
-        template.querySelector(".books__read-btn").href = array[i].accessInfo.webReaderLink
-        template.querySelector(".books__info-btn").dataset.infoId = array[i].id
-        template.querySelector(".books__read-btn").dataset.readId = array[i].id
-        
-        fragment.appendChild(template)
+        elWrapper.appendChild(fragment)
     }
-    elWrapper.appendChild(fragment)
 }
 
 
@@ -168,12 +172,14 @@ elWrapper.addEventListener("click" , function (evt) {
 let elOrder = document.querySelector(".result__btn")
 
 elOrder.addEventListener("click" , function () {
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=${elInput.value}&orderBy=newest`)
-    .then(req => req.json())
-    .then(data => {
-        render(data.items)
-        elResult.textContent = data.totalItems
-    })
+    if (elInput.value.length > 1) {
+        fetch(`https://www.googleapis.com/books/v1/volumes?q=${elInput.value}&orderBy=newest`)
+        .then(req => req.json())
+        .then(data => {
+            render(data.items)
+            elResult.textContent = data.totalItems
+        })
+    }
 })
 
 elBookmarkWrapper.addEventListener("click" , function (evt) {
